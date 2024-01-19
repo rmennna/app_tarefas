d0e916c0fd95292a6d4e48c0340f02db5d453c69
Rodrigo Menna <rmenna.barroso@gmail.com>
2024-01-19 17:27:59 -0400

    TextFormField, Controller, ErrorBuilder and Image

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_tarefas/screens/form_screen.dart';
 import 'package:app_tarefas/screens/inicial_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const InitialScreen(),
+      home: const FormScreen(),
     );
   }
 }
